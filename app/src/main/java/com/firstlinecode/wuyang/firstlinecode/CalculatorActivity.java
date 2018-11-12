cef61c50f94c88812259f132c83a2617c97cfa26
武扬 <956826374@qq.com>
2018-11-12 17:32:53 +0800

    完成计算机的布局

---
+++ app/src/main/java/com/firstlinecode/wuyang/firstlinecode/CalculatorActivity.java
@@ -0,0 +1,12 @@
+package com.firstlinecode.wuyang.firstlinecode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
