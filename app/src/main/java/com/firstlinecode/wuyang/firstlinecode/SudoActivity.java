ce3d61f9cbcf78416905216931a1c90e501b390d
武扬 <956826374@qq.com>
2018-11-13 13:44:35 +0800

    重构了初始化页面

---
+++ app/src/main/java/com/firstlinecode/wuyang/firstlinecode/SudoActivity.java
@@ -0,0 +1,13 @@
+package com.firstlinecode.wuyang.firstlinecode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SudoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sudo);
+    }
+}
